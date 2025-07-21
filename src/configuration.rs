5c41e3d29e94c5f65665d09be032f3744914f0a2
nyavro <e.nyavro@gmail.com>
2025-07-21 17:58:10 +0700

    Cofiguratio implemented

---
+++ src/configuration.rs
@@ -0,0 +1,25 @@
+use serde::Deserialize;
+use figment::{Figment, providers::{Format, Toml, Json, Env}};
+use std::collections::HashMap;
+
+#[derive(Deserialize, Clone)]
+pub struct ProviderConfig {
+    pub method: String,
+    pub url: String,
+    pub headers: HashMap<String, String>,
+    pub form: HashMap<String, String>
+}
+
+#[derive(Deserialize, Clone)]
+pub struct Config {
+    pub auth_providers: HashMap<String, ProviderConfig>,
+    pub redirect_url: String
+}
+
+pub fn load_configuration() -> Result<Config, figment:Error> {
+    Ok(
+        Figment::new()
+            .join(Json::file("config.json"))
+            .extract()?
+    )    
+}
